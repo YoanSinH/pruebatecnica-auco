9844824e0ee0b6fcfec1c041bfb2e61156cafb05
yoansinh <joancifuentes@outlook.es>
2023-09-10 17:16:07 -0500

    Added fetch post function

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,20 @@
+'use client'
+import React from 'react'
 import Card from '@/components/Card'
-import Image from 'next/image'
 
 export default function Home() {
+  const [posts,setPosts] = React.useState([])
+
+  const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await response.json()
+    setPosts(data)
+  }
+
+  React.useEffect(() => {
+    fetchPosts()
+  },[])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Posts</h1>
