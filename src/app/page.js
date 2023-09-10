6e74eb6c125dd1669a810fdf7265fbf9aee0ea21
yoansinh <joancifuentes@outlook.es>
2023-09-10 17:07:36 -0500

    Added id prop to card component

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Posts</h1>
-      <Card title="Titleeeee" author="Authorrrrr" content="Contentttt"/>
+      <Card id="11" title="Titleeeee" author="Authorrrrr" content="Contentttt"/>
     </main>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,10 @@
 export default function Card(props) {
+    const id = props.id
     const title = props.title
     const author = props.author
     const content = props.content
     return (
-        <a href="#" className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-800 dark:border-zinc-700">
+        <a id={id} href="#" className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-zinc-800 dark:border-zinc-700">
             <div>
                 <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
                 <p className="mb-2 font-light text-gray-600 dark:text-gray-400">{author}</p>
