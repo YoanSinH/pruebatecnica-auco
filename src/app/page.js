2735e1957344532119a553db262b892e06332d66
yoansinh <joancifuentes@outlook.es>
2023-09-10 16:58:10 -0500

    Added card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Card from '@/components/Card'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Posts</h1>
+      <Card title="Titleeeee" author="Authorrrrr" content="Contentttt"/>
     </main>
   )
 }
