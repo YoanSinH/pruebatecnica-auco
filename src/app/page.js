e4e7b3ebc3f67ea6f0a2ccc5aebc3463b561ab6c
yoansinh <joancifuentes@outlook.es>
2023-09-11 16:40:53 -0500

    Added Navbar component

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import Card from '@/components/Card'
 import {getPostsAndAuthors} from '@/services/fetchPosts'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   const [data,setData] = React.useState([])
@@ -13,10 +14,17 @@ export default function Home() {
   },[])
 
   return (
+    <>
+      <Navbar/>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className='font-bold text-3xl'></h1>
+      <h2 className="font-semibold text-3xl mb-4">Posts 📝</h2>
+      <div className="grid grid-cols-1 gap-2 md:grid-cols-3">
       {data.map(post => (
         <Card key={post.id} id={post.id} title={post.title} author={post.author} content={post.body} contentPage={'/post/'+post.id}/>
       ))}
+      </div>
     </main>
+    </>
   )
 }
