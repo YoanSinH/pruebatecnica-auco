8ff963cb4d3ce024e95459382c8a2989e4037041
yoansinh <joancifuentes@outlook.es>
2023-09-12 16:54:26 -0500

    Added pagination

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,27 @@ import CardSkeleton from '@/components/CardSkeleton'
 import Footer from '@/components/Footer'
 
 export default function Home() {
+  const bottomRef = React.useRef(null);
   const [data,setData] = React.useState([])
+  const [currentPage] = React.useState(1);
+  const [dataPerPage, setDataPerPage] = React.useState(9);
   
   React.useEffect(() => {
     getPostsAndAuthors()
     .then((data) => setData(data))
     .catch((error) => console.error('Error:', error));
   },[])
+
+  const loadData = () => {
+    setDataPerPage(dataPerPage + 9);
+    setTimeout(() => {
+      bottomRef.current.scrollIntoView({ behavior: "smooth" });
+    }, 100);
+  }
+
+  const indexLastData = currentPage * dataPerPage;
+  const indexFistData = indexLastData - dataPerPage;
+  const currentData = data.slice(indexFistData, indexLastData);
 
   return (
     <>
@@ -31,10 +45,14 @@ export default function Home() {
       <CardSkeleton/>
       </>
         :
-      data.map(post => (
+      currentData.map(post => (
         <Card key={post.id} id={post.id} title={post.title} author={post.author} content={post.body} contentPage={'/post/'+post.id}/>
       ))}
       </div>
+      <div className='mt-5 flex items-center justify-center'>
+        <button type="button" className="items-center text-zinc-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700" onClick={loadData}>Cargar Más</button>
+      </div>
+      <div ref={bottomRef} />
     </main>
     <Footer/>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js" async></script>
