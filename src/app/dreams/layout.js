6789bd61ccc3d302138e12211495b3c783f8db25
yoansinh <joancifuentes@outlook.es>
2023-09-12 18:08:23 -0500

    added dreams page

---
+++ src/app/dreams/layout.js
@@ -0,0 +1,8 @@
+export default function Layout({ children }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js" async></script>
+      </html>
+    )
+}
