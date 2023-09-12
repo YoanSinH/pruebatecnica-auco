91d11122d720274b3101902547b6b52a7637f9b5
yoansinh <joancifuentes@outlook.es>
2023-09-12 18:46:25 -0500

    Added markdown text

--- src/app/dreams/page.js
+++ src/app/dreams/page.js
@@ -1,5 +1,14 @@
+import Navbar from "@/components/Navbar";
+import { MySelf } from "@/data/myself";
+
 export default function Page(){
     return (
-    <h1>hola</h1>
+        <>
+        <Navbar currentPage="dreams"/>
+        <main className="flex min-h-screen flex-col mt-10 md:px-24 px-5">
+            <h1>aa</h1>
+            <div dangerouslySetInnerHTML={{__html: MySelf()}}/>
+        </main>
+        </>
     )
 }
---
+++ src/data/myself.js
@@ -0,0 +1,31 @@
+export const MySelf = () => {
+    let text = `
+    Me gustaría dedicar esta página a compartir mis sueños y proyecciones a futuro. Auco no es solo un lugar de trabajo para mí; es un espacio donde puedo crecer, aprender y contribuir de manera significativa. Aquí están mis aspiraciones y metas profesionales, así como mi visión de cómo puedo aportar al éxito continuo de Auco.
+    
+    ## Aspiraciones Profesionales:
+    
+    **Desarrollo Continuo:** Uno de mis sueños más profundos es convertirme en un profesional altamente competente y en constante evolución. Deseo seguir aprendiendo y adquiriendo nuevas habilidades para ser un recurso valioso para el equipo y para la empresa en su conjunto.
+    
+    **Liderazgo:** A medida que mi experiencia y conocimiento crezcan, aspiro a desempeñar un papel de liderazgo dentro de Auco. Quiero inspirar a mis colegas y ayudarlos a alcanzar su máximo potencial, contribuyendo así al éxito colectivo de la empresa.
+    
+    **Innovación:** Sueño con ser parte de un equipo que constantemente busca nuevas formas de abordar desafíos y mejorar procesos. Deseo contribuir activamente a la cultura de la innovación en Auco y ser un impulsor de soluciones creativas.
+    
+    ## Metas a Corto y Largo Plazo:
+    
+    **Corto Plazo:** En los próximos 1-2 años, me enfocaré en consolidar mi experiencia en mi rol actual y contribuir de manera efectiva a los proyectos en los que estoy involucrado. También planeo participar en programas de capacitación y desarrollo para fortalecer mis habilidades.
+    
+    **Largo Plazo:** A medida que mire hacia el futuro a largo plazo, mi objetivo es asumir roles de mayor responsabilidad y liderazgo dentro de Auco. Esto podría incluir la gestión de equipos, la supervisión de proyectos estratégicos y la toma de decisiones clave para el crecimiento de la empresa.
+    
+    ## Contribución a la Empresa:
+    Creo firmemente que mi contribución a Auco puede ser significativa en varios aspectos:
+    
+    Excelencia en el Trabajo: Me comprometo a entregar un trabajo de alta calidad en todo momento. Busco la mejora continua en mis tareas diarias para garantizar que Auco continúe siendo un referente en nuestro sector.
+    
+    Colaboración: Trabajaré en estrecha colaboración con mis colegas, compartiendo conocimientos y brindando apoyo cuando sea necesario. Creo que un equipo fuerte es la base del éxito de cualquier empresa.
+    
+    Innovación: Estoy dispuesto a aportar ideas innovadoras y a colaborar en proyectos que impulsen la eficiencia y la innovación en Auco. Creo que la adaptación constante a las cambiantes demandas del mercado es esencial.
+    
+    En resumen, mi sueño es ser un miembro valioso y comprometido de Auco, contribuyendo al crecimiento y éxito continuo de la empresa. Estoy emocionado por las oportunidades que el futuro nos depara y ansioso por trabajar junto a todos ustedes para hacer realidad estos sueños y metas.    
+    `
+    return text
+}
