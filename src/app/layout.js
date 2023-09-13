ca07fc9ca79261c3eeb3094c4a189cb6b5aad489
yoansinh <joancifuentes@outlook.es>
2023-09-13 12:15:26 -0500

    Changed brand

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Posty Page - Prueba Tecnica Auco',
+  title: 'AucoPost - Prueba Tecnica Auco',
   description: 'Prueba tecnica para Auco',
 }
 
